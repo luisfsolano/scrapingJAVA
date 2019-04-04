829442a5b00a0e40e157f3a5eae418557990d3e3
luisfsolano <luissolano13@gmail.com>
2019-04-04 12:57:35 -0500

    metodos faltantes

--- src/main/java/com/luisfsolano/app/NewMain.java
+++ src/main/java/com/luisfsolano/app/NewMain.java
@@ -23,25 +23,28 @@ public class NewMain {
      */
     public static void main(String[] args) {
         
-//        Scanner scaner = new Scanner(System.in);
-//        
-//        System.out.println("Ingrese el alias de la tarjeta");
-//        String alias = scaner.nextLine();
-//        
-//        System.out.println("ingrese el No. de tarjeta");
-//        Integer id = scaner.nextInt();
-        
         TarjetaController tController = new TarjetaController();
-//        Tarjeta tarjeta = tController.crearTarjeta(32350141,"yarelis");
+        
+        Scanner scaner = new Scanner(System.in);
+//        
+        System.out.println("Ingrese el alias de la tarjeta");
+        String alias = scaner.nextLine();
+        
+        System.out.println("ingrese el No. de tarjeta");
+        Integer id = scaner.nextInt();
+        
+        
+        Tarjeta tarjeta = tController.crearTarjeta(id,alias);
 //        Tarjeta tarjeta = tController.BuscarTarjeta(32350141);
-//        System.out.println("id: \""+tarjeta.getIdTarjeta()+"\" alias: \""+tarjeta.getAlias()+"\" saldo: \""+tarjeta.getSaldo()+"\"");
         
-        List<Tarjeta> tarjetas = new ArrayList<>();
-        tarjetas.addAll(tController.ListarTodas());
+        System.out.println("id: \""+tarjeta.getIdTarjeta()+"\" alias: \""+tarjeta.getAlias()+"\" saldo: \""+tarjeta.getSaldo()+"\"");
         
-        for (Tarjeta tarjeta : tarjetas) {
-            System.out.println("id: \""+tarjeta.getIdTarjeta()+"\" alias: \""+tarjeta.getAlias()+"\" saldo: \""+tarjeta.getSaldo()+"\"");
-        }
+//        List<Tarjeta> tarjetas = new ArrayList<>();
+//        tarjetas.addAll(tController.ListarTodas());
+//        
+//        for (Tarjeta tarjeta : tarjetas) {
+//            System.out.println("id: \""+tarjeta.getIdTarjeta()+"\" alias: \""+tarjeta.getAlias()+"\" saldo: \""+tarjeta.getSaldo()+"\"");
+//        }
         
         System.exit(0); 
     }
